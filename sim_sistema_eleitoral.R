9bd4e7bc6e786d56b0f4001af19664b6c4511a18
mgaldino <mcz.fea@gmail.com>
2016-08-03 17:27:48 -0300

    Funções finalizadas, mas parece ter um bug, pois sempre os mesmos candidatos são escolhidos.

--- sim_sistema_eleitoral.R
+++ sim_sistema_eleitoral.R
@@ -17,69 +17,6 @@
 # espaço unidimensional (quão próximo está do eleitor)
 
 
-## fun gera eleitores
-gera_eleitores < function(n=5000) {
-  library(MASS)
-  # Simulate bivariate normal data
-  mu <- c(0,0)                         # Mean
-  Sigma <- matrix(c(.8, .3, .3, .8), 2)  # Covariance matrix
-  eleitores <- mvrnorm(n, mu = mu, Sigma = Sigma )  # from Mass package
-  eleitores <- as.data.frame(eleitores)
-}
-
-## fun gera candidatos
-gera_candidatos <- function(k=100) {
-  library(MASS)
-  # Simulate bivariate normal data
-  mu <- c(0,0)
-  Sigma_cand <- matrix(c(1, .6, .6, 1), 2)
-  candidatos <- mvrnorm(k, mu = mu, Sigma = Sigma_cand )  # from Mass package
-  candidatos <- as.data.frame(candidatos)
-}
-
-# plataforma do honesto
-plat_honesto <- function() {
-  candidato
-}
-
-gera_plataformas <- function(k=100, prop.tipos = rep(.25, 4)) {
-  plat <- matrix(nrow=100, ncol=2)
-  stopifnot(sum(prop.tipos) <= 1 & all(prop.tipos) >= 0)
-  size <- cumsum(floor(prop.tipos*k))
-  if(max(size) > k) size[4] <- 100
-  for ( i in 1:k) {
-    if(i <= size[1]) {
-      plat[i,] <- candidatos[i,] ## honesto
-    } else {
-      if ( i <= size[2]) {
-        
-      }
-    }
-    
-
-  }
-  
-}
-
-
-## coleta info eleitor
-pesq_eleitor <- function(s=10, n=5000, eleitores) {
-  # s é o tamanho da amostra e reflete dinheiro arrecadado
-  pesq <- matrix(nrow=k, ncol=2)
-  pesq[j,] <- colMeans(eleitores[sample(1:n, s),])
-}
-
-
-# plataforma do mentiroso
-plat_mentiroso <- function(k, n, s=10, eleitores) {
-  pesq <- pesq_eleitor(s, n)
-  plat <- matrix(nrow=100, ncol=2)
-  stopifnot(alpha <= 1 & alpha >= 0)
-  size <- floor(alpha*k)
-  for ( j in 1:size) {
-    plat[j,] <- candidatos[j,]
-  }
-}
 
 # candidato observa média amostral (pequena amostra) das duas dimensões,
 # várias regras (tipos de candidatos)
@@ -90,79 +27,153 @@ plat_mentiroso <- function(k, n, s=10, eleitores) {
 
 
 # honesto anuncia x
-
-
 # mentiroso, anuncia media_eleitor
 
-
-# 1. coleta info s/ eleitor: 
-# s = f($), tamanho da amostra ou info sobre o eleitor
-s <- 10
-pesq <- matrix(nrow=k, ncol=2)
-pesq[j,] <- colMeans(eleitores[sample(1:n, s),])
-
-## mentiroso, anuncia resultado de pesq
-plat[j,] <- pesq[j,]
-
-Volúvel e resistente
-# 2. computa diferença 
-dif[j] <- candidatos[j,] - pesq[j,]
-# 3. anuncia plataforma se resistente
-keep.indx <- which.min(candidatos[j,] - pesq[j,])
-keep <- pesq[j,keep.indx]
-
-plat[j,] <- candidatos[j,]
-if (keep.indx == 1) {
-  plat[j,2] <- keep
-} else {
-  plat[j,1] <- keep
+## fun gera eleitores
+gera_eleitores <- function(n) {
+  library(MASS)
+  # Simulate bivariate normal data
+  mu <- c(0,0)                         # Mean
+  Sigma <- matrix(c(.8, .3, .3, .8), 2)  # Covariance matrix
+  eleitores <- mvrnorm(n, mu = mu, Sigma = Sigma )  # from Mass package
+  eleitores <- as.data.frame(eleitores)
+  colnames(eleitores) <- c("dim1", "dim2")
+  return(eleitores)
 }
 
-# 3. anuncia plataforma se volúvel
-keep.indx <- which.max(candidatos[j,] - pesq[j,])
-keep <- pesq[j,keep.indx]
-
-plat[j,] <- candidatos[j,]
-if (keep.indx == 1) {
-  plat[j,2] <- keep
-} else {
-  plat[j,1] <- keep
+## fun gera candidatos
+gera_candidatos <- function(k) {
+  library(MASS)
+  # Simulate bivariate normal data
+  mu <- c(0,0)
+  Sigma_cand <- matrix(c(1, .6, .6, 1), 2)
+  candidatos <- mvrnorm(k, mu = mu, Sigma = Sigma_cand )  # from Mass package
+  candidatos <- as.data.frame(candidatos)
+  names(candidatos) <- c("pos_dim1", "pos_dim2")
+  return(candidatos)
 }
 
-## eleitor
-## por enquanto, ouve aleatoriamente um candidato
-c <- 5 # quantos candidatos procura
-cand <- sample(1:k, c)
-Sigma <- matrix(c(.01, .005, .005, .01), 2)
+## coleta info eleitor
+pesq_eleitor <- function(s=10, n=5000, eleitores) {
+  # s é o tamanho da amostra e reflete dinheiro arrecadado
+  pesq <- colMeans(eleitores[sample(1:n, s),])
+}
 
-# del mede quão alienado é o eleitor
-# quanto menor del, menos ele é alienado
+## candidatos definem plataforma
+gera_plataformas <- function(k, n,
+                             prop.tipos = rep(.25, 4),
+                             candidatos_true, poll) {
+  dif <- abs(candidatos - poll)
+  
+  ## gerar plataforma
+  plat <- matrix(nrow=k, ncol=2)
+  size <- cumsum(floor(prop.tipos*k))
+  if(max(size) > k)  { size[4] <- 100 } ## corrige arredondamento, pode dar 101, por exemplo.
+  
+  ## honesto
+  for ( i in 1:size[1]) {
+      plat[i,] <- unlist(candidatos[i,])
+    } 
+  
+  ## mentiroso
+  for ( i in (size[1]+1):size[2]) {
+    plat[i,] <- unlist(poll) ## nesta versão não tem diferença entre candidatos
+  } 
+    
+  ## muda pouco (muda na dim dele mais próxima do mediano )
 
-del <- 1
+  for ( i in (size[2]+1):size[3]) {
+      plat[i,] <- unlist(candidatos[i,])
+      aux <- which.min(dif[i,])
+      plat[i,aux] <- unlist(poll[aux])
+    }
 
+  
+  ## muda-muito (muda na dim dele mais distanto do mediano )
+  for ( i in (size[3]+1):size[4]) {
+    plat[i,] <- unlist(candidatos[i,])
+    aux <- which.max(dif[i,])
+    plat[i,aux] <- unlist(poll[aux])
+  } 
+  return(plat)
+}
 
-# cria vetor para guardar escolha do voto
-voto <- matrix(c(1:n, rep(NA, n)), nrow=n, ncol=2)
-
-# cria vetor para guardar cálculo de função de perda
-loss_vec <- numeric()
-
-for ( j in 1:c) {
-  mu <- plat[cand[j],]
-  plat_eleitor <- mvrnorm(1, mu = mu, Sigma = Sigma )  # from Mass package
-loss <- sqrt(sum((plat_eleitor - eleitores[i,])^2))
-    if ( loss <= del) {
-    # vota no candidato
-      voto[i,2] <- cand[j]
-    break
-  } else {
-    # procura outro candidato
-    loss_vec[j] <- loss
+gera_voto <- function(n, c, k, vec_prob, init_candidatos, eleitores) {
+  voto <- numeric()
+  for (i in 1:n) {
+    x <- eleitores[i,]
+    index_match <- which(rbinom(k, c, vec_prob)==1)
+    size_vec <- length(index_match)
+    while (size_vec == 0) {
+      index_match <- which(rbinom(k, c, vec_prob)==1)
+      size_vec <- length(index_match)
+    }
+    aux <- matrix(rep(unlist(x),size_vec), nrow=size_vec, ncol=2, byrow=T)
+    aux2 <- init_candidatos[index_match,]
+    dif_elec <- aux - aux2
+    loss <- numeric()
+    #print(size_vec)
+    for ( j in 1:size_vec) {
+      #print(paste(paste(i, "este foi i; j é"), j))
+      loss[j] <- sqrt(sum(dif_elec[j,]^2))
+    }
+    voto[i] <- index_match[which.min(loss)]
   }
+  return(voto)
 }
 
-if (is.na(voto[i,2])) {
-  voto[i,2] <- cand[which.min(loss_vec)] 
+computa_eleitos <- function(df_voto, n_cadeiras) {
+  resultado <- as.data.frame(table(df_voto))
+  resultado <- resultado[order(-resultado$Freq),]
+  eleitos <- head(resultado, n_cadeiras)
+  return(eleitos)
+}
+
+realiza_eleicoes <- function (n, k, prop.tipos, vec_prob, c ) {
+  voters <- gera_eleitores(n=n)
+
+  candidatos_true <- gera_candidatos(k=k)
+
+  mediano_amostral <- pesq_eleitor(s=10, eleitores = voters)
+  plat <- gera_plataformas(k=k_cand, candidatos_true, poll=mediano_amostral)
+
+  voto <- gera_voto(n=n, k=k, c=c, init_candidatos=plat,
+                    vec_prob = vec_prob, eleitores = voters)
+  
+  votacao <- computa_eleitos(df_voto = voto, n_cadeiras = num_cadeiras)
+  eleitos <- as.numeric(as.character(unlist(votacao[, 1])))
+  return(eleitos)
 }
 
 
+## Sim eleição inicial
+
+## inicializa variáveis
+n_elect <- 10000 # num eleitores
+k_cand <- 100 # num candidatos
+vec_prob_init <- (1:k)/k # prob do candidato contatar eleitor
+num_cadeiras <- 10
+n.sim <- 100 ## número de eleições
+delta <- .01
+
+# primeira eleição
+eleitos <- realiza_eleicoes(n=n_elect, k=k_cand, prop.tipos=rep(.25, 4), 
+                            vec_prob=vec_prob_init, c=num_cadeiras)
+
+history_elections <- matrix(nrow=n.sim, ncol=num_cadeiras)
+history_elections[1, ] <- eleitos
+# segunda em diante
+cand_aux <- 1:k
+for ( i in 2:n.sim) {
+
+  vec_prob_init[cand_aux[eleitos]] <- vec_prob_init[cand_aux[eleitos]]+ delta
+  # normaliza
+  vec_prob_init <- vec_prob_init/sum(vec_prob_init)
+  history_elections[i, ] <- realiza_eleicoes(n=n_elect, k=k_cand, prop.tipos=rep(.25, 4), 
+                              vec_prob=vec_prob_init, c=num_cadeiras)
+  print(i)
+}
+head(history_elections)
+history_elections1 <- t(apply(t(history_elections), 2, sort))
+head(history_elections1)
+View(history_elections1)
